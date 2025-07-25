f40e80715bdc7475c558989c87a438c2a3188934
Guerineau Tom <124735919+Tomguerine@users.noreply.github.com>
2025-07-25 17:24:19 +0200

    feat: require secret key in env

--- server/middleware/tokenValidation.js
+++ server/middleware/tokenValidation.js
@@ -9,10 +9,7 @@ module.exports.validateToken = (req, res, next) => {
     }
 
     const userToken = req.headers.authorization.split('Bearer')[1].trim()
-    const decodedToken = jwt.verify(
-      userToken,
-      process.env.SECRET_KEY || 'default-secret-key'
-    )
+    const decodedToken = jwt.verify(userToken, process.env.SECRET_KEY)
     req.user = decodedToken
     return next()
   } catch (error) {
