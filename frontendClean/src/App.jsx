052503b7d6fe484c74f90cfdb000762587ec2656
Guerineau Tom <124735919+Tomguerine@users.noreply.github.com>
2025-08-06 20:44:11 +0200

    feat: add protected user page

--- frontendClean/src/App.jsx
+++ frontendClean/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage.jsx';
 import NotFound from './components/NotFound.jsx';
+import UserPage from './components/UserPage.jsx';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/user" element={<UserPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
